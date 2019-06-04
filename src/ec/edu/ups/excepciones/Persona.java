4b8c5868e7c794328fb1cfe91c6cb2947e8b4082
Byron PC <Byron PC@Internet>
2019-06-03 20:59:47 -0500

    Inicio de Programa

---
+++ src/ec/edu/ups/excepciones/Persona.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.excepciones;
+
+/**
+ *
+ * @author Byron PC
+ */
+public class Persona {
+    private String cedula;
+    private String nombre;
+    private String apellido;
+    private int edad;
+
+    public Persona() {
+    }
+    
+}
